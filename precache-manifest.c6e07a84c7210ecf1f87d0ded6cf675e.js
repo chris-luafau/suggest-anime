88de52c7f40ba2be28bc552756c22638d0f6bfa0
Chris Luafau <31860645+chris-luafau@users.noreply.github.com>
2019-10-28 17:22:26 -0700

    Updates

---
+++ precache-manifest.c6e07a84c7210ecf1f87d0ded6cf675e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebb8e6c93e75989d527924e17b5e760",
+    "url": "/suggest-anime/index.html"
+  },
+  {
+    "revision": "68a2a2c307f7ec996a9e",
+    "url": "/suggest-anime/static/js/2.909cce6d.chunk.js"
+  },
+  {
+    "revision": "ae36bc2393fe40a0a2ec",
+    "url": "/suggest-anime/static/js/main.3738148b.chunk.js"
+  },
+  {
+    "revision": "e4837f2a02152a92eda5",
+    "url": "/suggest-anime/static/js/runtime-main.b5a2e426.js"
+  }
+]);
